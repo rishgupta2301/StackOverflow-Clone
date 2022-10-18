9c44c3b6d7336591c858329ec3c5cb9181a0ae4e
rishgupta2301 <guptarishabh1122@gmail.com>
2022-10-18 18:11:21 +0530

    Styled RightSidebar

--- client/src/components/RightSidebar/Widget.jsx
+++ client/src/components/RightSidebar/Widget.jsx
@@ -10,26 +10,26 @@ const Widget = () => {
       <h4>The Overflow Blog</h4>
       <div className="right-sidebar-div-1">
         <div className="right-sidebar-div-2">
-          <img src={pen} alt='pen' width='18'/>
+          <img style={{width:'15px', marginTop:'3px'}} src={pen} alt='pen' width='18'/>
           <p>Observability is key to the future of software (and your DevOps career) </p>
         </div>
         <div className="right-sidebar-div-2">
-          <img src={pen} alt='pen' width='18'/>
+          <img style={{width:'15px', marginTop:'3px'}} src={pen} alt='pen' width='18'/>
           <p>Podcast 374: How valuable is your screen name? </p>
         </div>
       </div>
       <h4>Featured on Meta</h4>
       <div className="right-sidebar-div-1">
         <div className="right-sidebar-div-2">
-          <img src={comment} alt='pen' width='18'/>
+          <img style={{width:'15px', marginTop:'3px'}} src={comment} alt='pen' width='18'/>
           <p style={{marginRight:'25px'}}>Review queue workflows - Final release...</p>
         </div>
         <div className="right-sidebar-div-2">
-          <img src={comment} alt='pen' width='18'/>
+          <img style={{width:'15px', marginTop:'3px'}} src={comment} alt='pen' width='18'/>
           <p>Please welcome Valued Associates: #958-V2Blast #959-SpencerG</p>
         </div>
         <div className="right-sidebar-div-2">
-          <img src={blackLogo} alt='pen' width='18'/>
+          <img style={{width:'15px', marginTop:'3px'}} src={blackLogo} alt='pen' width='18'/>
           <p>Outdated Answers: accepted answer is now unpinned on Stack Overflow</p>
         </div>
       </div>
