c2f7a5cc1ec8defbbcd4f0c883613a6909c701dd
rishgupta2301 <guptarishabh1122@gmail.com>
2022-11-14 22:37:33 +0530

    Completed editprofile page

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,11 +1,14 @@
 import express from "express";
 import { login, signup } from "../controllers/auth.js";
-import {getAllUsers} from '../controllers/users.js'
+import {getAllUsers,updateProfile} from '../controllers/users.js'
+import auth from '../middlewares/auth.js'
 
 const router = express.Router();
 
 router.post('/signup',signup)  // here signup is used as a callback function
 router.post('/login', login)  // here login is used as a callback function
-router.get('/getAllUsers',getAllUsers)
+
+router.get('/getAllUsers', getAllUsers)
+router.patch('/update/:id',auth,updateProfile)
 
 export default router;
